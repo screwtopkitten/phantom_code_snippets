e00dba15e9ccc90b31bec93bdf1fe4344406f1d5
screwtopkitten <72877287+screwtopkitten@users.noreply.github.com>
2021-09-08 11:34:40 +0000

    Update dynamic_input_basic.py

--- basic_python_playbook/dynamic_input_basic.py
+++ basic_python_playbook/dynamic_input_basic.py
@@ -40,15 +40,5 @@ def geolocate_ip_cb(action=None,success=None,container=None,results=None,handle=
 def on_finish(container, summary):
     phantom.debug('on_finish() called')
     phantom.debug(summary)
-    # This function is called after all actions are completed.
-    # summary of all the action and/or all detals of actions
-    # can be collected here.
-
-    # summary_json = phantom.get_summary()
-    # if 'result' in summary_json:
-        # for action_result in summary_json['result']:
-            # if 'action_run_id' in action_result:
-                # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
-                # phantom.debug(action_results)
 
     return
