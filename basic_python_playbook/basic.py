a6ae04a61f20b7c40281d83a8b17ef092b27442b
screwtopkitten <72877287+screwtopkitten@users.noreply.github.com>
2021-09-08 11:27:36 +0000

    Update basic.py

--- basic_python_playbook/basic.py
+++ basic_python_playbook/basic.py
@@ -5,7 +5,6 @@ import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
 def on_start(container):
-    name= {"name":"container name"}
     phantom.debug('on_start() called')
     phantom.debug(container)
             
